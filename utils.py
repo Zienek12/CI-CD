9be5e24eee19fbef3d3284963ae081650596839d
Adrian Zienkiewicz <154380633+Zienek12@users.noreply.github.com>
2026-05-28 15:35:52 +0200

    used balck to reformat files

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 """Utilities: proste operacje arytmetyczne."""
 
+
 def add(a: int, b: int) -> int:
     """Zwraca sumę dwóch liczb całkowitych.
 
@@ -12,13 +13,16 @@ def add(a: int, b: int) -> int:
     """
     return a + b
 
+
 def subtract(a: int, b: int) -> int:
     """Zwraca różnicę `a - b`."""
     return a - b
 
+
 def multiply(a: int, b: int) -> int:
     """Zwraca iloczyn `a * b`."""
     return a * b
+
 
 def divide(a: int, b: int) -> float:
     """Dzieli `a` przez `b` i zwraca wynik jako float.
